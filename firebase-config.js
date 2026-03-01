32c1a9d2cd5dfeb2ff7ac2b3aa4bd95a86e9a51c
ppdinamica <ppdinamica@gmail.com>
2026-03-01 12:23:19 -0800

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkRYHgRYTX7m31umvVNH_IDrEepwKIw4I",
+  authDomain: "materiales-status.firebaseapp.com",
+  projectId: "materiales-status",
+  storageBucket: "materiales-status.appspot.com",
+  messagingSenderId: "126049792327",
+  appId: "1:126049792327:web:01197c9fc37d7e4b6edc62"
+};
