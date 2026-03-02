1a7c328030712e0de6479fda545a9a53ad363bc6
ppdinamica <ppdinamica@gmail.com>
2026-03-02 01:21:39 -0800

    Update Firebase storage bucket URL

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyCkRYHgRYTX7m31umvVNH_IDrEepwKIw4I",
   authDomain: "materiales-status.firebaseapp.com",
   projectId: "materiales-status",
-  storageBucket: "materiales-status.appspot.com",
+  storageBucket: "materiales-status.firebasestorage.app",
   messagingSenderId: "126049792327",
   appId: "1:126049792327:web:01197c9fc37d7e4b6edc62"
 };
